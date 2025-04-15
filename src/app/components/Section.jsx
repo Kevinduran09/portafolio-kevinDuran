50131f693a1c0c368b3ea34a0cb7c9586f49da86
Kevin Andrey Durán Martínez <kevinduran.net.123@gmail.com>
2025-04-14 20:50:44 -0600

    add carrousel and fix texts

--- src/app/components/Section.jsx
+++ src/app/components/Section.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export const Section = ({ id, className, children, props, lgW ='max-w-4xl', mdW ='max-w-4xl'}) => {
+export const Section = ({ id, className, children, props, lgW = 'max-w-4xl', mdW = 'max-w-4xl' }) => {
   return (
-    <section id={id} className={`mx-auto w-full lg:${lgW} md:${mdW} container ${className}`} {...props}> 
-        {children}
+    <section
+      id={id}
+      className={`mx-auto w-full ${lgW ? `lg:${lgW}` : 'lg:max-w-4xl'} ${mdW ? `md:${mdW}` : 'md:max-w-4xl'} container ${className}`}
+      {...props}
+    >
+      {children}
     </section>
   )
 }
